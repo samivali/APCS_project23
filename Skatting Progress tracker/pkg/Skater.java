b28b150edd363013f63ada9464f42e44a7073728
EC2 Default User <ec2-user@ip-172-31-90-140.ec2.internal>
2023-05-30 21:11:00 +0000

    classes complete, final product

--- Skatting Progress tracker/pkg/Skater.java
+++ Skatting Progress tracker/pkg/Skater.java
@@ -1,38 +1,58 @@
-/*package pkg;
+package pkg;
 import java.util.*;
 
-public class Skater extends Trick{
+public class Skater{
     Trick [] list = new Trick[11];
+    int level;
+    ArrayList<Trick> work;
     
     public Skater(){
         level = 1;
+        list [0] = new Trick("Marching", "https://youtu.be/GAyMgZc8hJA", 1);
+        list [1] = new Trick("Two-foot-Glide", "https://youtu.be/9zJuwbVSmPY", 1);
+        list [2] = new Trick("Forward and Backward Swizzles", "https://youtu.be/r8VG-3tFBho", 1);
+        list [3] = new Trick("One Foot Glide", "https://youtu.be/hKnyLPAgBPQ", 2);
+        list [4] = new Trick("Two Foot Turns", "https://youtu.be/BCCXsY1RX0E", 2);
+        list [5] = new Trick("Two Foot Spins", "https://youtu.be/ZWmNKqOEmGA", 2);
+        list [6] = new Trick("Inside and Outside edge", "https://youtu.be/FR9RsC9nNH8", 2);
+        list [7] = new Trick("Forward and Backward Crossovers", "https://youtu.be/cyQZ8bauDvY", 3);
+        list [8] = new Trick ("Three Turns" , "https://youtu.be/njqJC85X8U0", 4);
+        list [9] = new Trick("Mohawks", "https://youtu.be/8MrF6AxeN1w", 4);
+        list [10] = new Trick("One Foot Spin", "https://youtu.be/zyat2c-5MXw", 4);
     }
     
-    
-    public void generateTricks(){
-        list [0] = new Trick("Marching", " ", 1);
-        list [1] = new Trick("Two-foot-Glide", " ", 1);
-        list [2] = new Trick("Forward and Backward Swizzles", " ", 1);
-        list [3] = new Trick("One Foot Glide", " ", 2);
-        list [4] = new Trick("Two Foot Turns", " ", 2);
-        list [5] = new Trick("Two Foot Spins", " ", 2);
-        list [6] = new Trick("Inside and Outside edge", " ", 2);
-        list [7] = new Trick("Forward and Backward Crossovers", " ", 3);
-        list [8] = new Trick ("Three Turns" , " ", 4);
-        list [9] = new Trick("Mohawks", " ", 4);
-        list [10] = new Trick("One Foot Spin", " ", 4);
+    public void findLevel(){
+        int min = 0;
+        for(int x = 0; x<(work.size()); x++){
+            min = min+((work.get(x)).getLevel());
+        }
+        System.out.println("Your level is around " + (min/(work.size())) + " on the ISI learning levels");
         
     }
     
     public void achivements(){
         String check;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("type T for all the skills you have");
         for(int x = 0; x<list.length; x++){
-            System.out.println("type T for all the skills you have");
 	        System.out.println(list[x].getName());
 	        check = sc.nextLine();
 	        if(check.equals("T")){
 	            list[x].setAchive();
 	        }
         }
+    }
+    
+    public void inProgress(){
+    	work = new ArrayList<Trick>();
+    	for(int x=0; x<list.length; x++){
+    		if(list[x].getAchive()==false){
+    			work.add(list[x]);
+    		}
+    	}
+    }
+    
+    public ArrayList<Trick> getProgress(){
+        return work;
     }
 }
--- Skatting Progress tracker/starter.java
+++ Skatting Progress tracker/starter.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 class starter {
 	
-	public static void achivements(Trick [] list){
+	/*public static void achivements(Trick [] list){
         String check;
         Scanner sc = new Scanner(System.in);
         System.out.println("type T for all the skills you have");
@@ -24,11 +24,11 @@ class starter {
     		}
     	}
     	return work;
-    }
+    }*/
 	
 	public static void main(String args[]) {
 
-		Trick [] list = new Trick[11];
+		/*Trick [] list = new Trick[11];
 		
         list [0] = new Trick("Marching", "https://youtu.be/GAyMgZc8hJA", 1);
         list [1] = new Trick("Two-foot-Glide", "https://youtu.be/9zJuwbVSmPY", 1);
@@ -40,15 +40,17 @@ class starter {
         list [7] = new Trick("Forward and Backward Crossovers", "https://youtu.be/cyQZ8bauDvY", 3);
         list [8] = new Trick ("Three Turns" , "https://youtu.be/njqJC85X8U0", 4);
         list [9] = new Trick("Mohawks", "https://youtu.be/8MrF6AxeN1w", 4);
-        list [10] = new Trick("One Foot Spin", "https://youtu.be/zyat2c-5MXw", 4);
-        
-        achivements(list);
+        list [10] = new Trick("One Foot Spin", "https://youtu.be/zyat2c-5MXw", 4);*/
+        Skater New  = new Skater();
+        New.achivements();
+        New.inProgress();
         System.out.println("You still need to work on: ");
-        ArrayList<Trick> work = new ArrayList<Trick>();
-        work = inProgress(list);
-        for(int x = 0; x< work.size(); x++){
-        	System.out.println((work.get(x)).getName() + " watch this and practice: " + (work.get(x)).getLink());
+        for(int x = 0; x<(New.getProgress()).size(); x++){
+            System.out.println("----------------------------------------------------------");
+        	System.out.println(((New.getProgress()).get(x)).getName() + ": watch this and practice: " + (((New.getProgress()).get(x)).getLink()));
         }
+        
+        New.findLevel();
         
 		
 	}
